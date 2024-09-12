65d7550f150e8e2c59fdd1b4f73a64dd1cac8574
Adilson Neubaner <151559307+Adilsonneubaner@users.noreply.github.com>
2024-09-11 23:15:45 -0300

    deploy

---
+++ src/hooks/useGet.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect, useContext } from "react"
+import { DataContext } from "../context/DataContext"
+export const useGet = (url) => {
+    const [loading, setLoading] = useState(false)
+    const [data, setData] = useState()
+    const {callGet} = useContext(DataContext)
+    
+    useEffect(() => {
+        const fechData = async () => {
+            setLoading(true)
+            try{
+                const res = await fetch(url)
+                const json = await res.json()
+                setData(json)
+            }catch(error){
+
+            }
+            setLoading(false)
+        }
+        fechData()
+    }, [url, callGet])
+   return {data,loading}
+}
