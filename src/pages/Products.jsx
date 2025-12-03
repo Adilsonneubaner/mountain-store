c17be2e3227c75d000e6b197fc1487961d802617
Adilson Neubaner <151559307+Adilsonneubaner@users.noreply.github.com>
2025-12-03 20:50:01 -0300

    corrigindo margem na página produtos

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -141,188 +141,173 @@ const Products = () => {
   return (
     <>
       <main>
-        <h1 id='title'>Navegue pelas categorias</h1>
-        <div id="container-category">
-
-          <div className="category">
-            <input type="radio" name="select-category" className='select-category' id='select-clothing' onClick={handleClothing}/>
-            <img src={vestuario} alt="imagem vestuário" className='img-category'/>
-            <p className="title-category">Vestuário</p>
+        <div className="products-page">
+          <h1 id='title'>Navegue pelas categorias</h1>
+          <div id="container-category">
+            <div className="category">
+              <input type="radio" name="select-category" className='select-category' id='select-clothing' onClick={handleClothing}/>
+              <img src={vestuario} alt="imagem vestuário" className='img-category'/>
+              <p className="title-category">Vestuário</p>
+            </div>
+            <div className="category">
+              <input type="radio" name="select-category" className='select-category' id='select-tools' onClick={handleTools}/>
+              <img src={ferramentas} alt="imagem ferramentas" className='img-category'/>
+              <p className="title-category">Ferramentas</p>
+            </div>
+            <div className="category">
+              <input type="radio" name="select-category" className='select-category' id='select-technology' onClick={handleTechnology}/>
+              <img src={tecnologia} alt="imagem tecnologia" className='img-category'/>
+              <p className="title-category">Tecnologia</p>
+            </div>
+            <div className="category">
+              <input type="radio" name="select-category" className='select-category' id='select-sport' onClick={handleSport}/>
+              <img src={esporte} alt="imagem esporte" className='img-category'/>
+              <p className="title-category">Esporte</p>
+            </div>
+            <div className="category">
+              <input type="radio" name="select-category" className='select-category' id='select-house' onClick={handleHouse}/>
+              <img src={casa} alt="imagem casa e móveis" className='img-category'/>
+              <p className="title-category">Casa e Móveis</p>
+            </div>
           </div>
 
-          <div className="category">
-            <input type="radio" name="select-category" className='select-category' id='select-tools' onClick={handleTools}/>
-            <img src={ferramentas} alt="imagem ferramentas" className='img-category'/>
-            <p className="title-category">Ferramentas</p>
-          </div>
-
-          <div className="category">
-            <input type="radio" name="select-category" className='select-category' id='select-technology' onClick={handleTechnology}/>
-            <img src={tecnologia} alt="imagem tecnologia" className='img-category'/>
-            <p className="title-category">Tecnologia</p>
-          </div>
-
-          <div className="category">
-            <input type="radio" name="select-category" className='select-category' id='select-sport' onClick={handleSport}/>
-            <img src={esporte} alt="imagem esporte" className='img-category'/>
-            <p className="title-category">Esporte</p>
-          </div>
-
-          <div className="category">
-            <input type="radio" name="select-category" className='select-category' id='select-house' onClick={handleHouse}/>
-            <img src={casa} alt="imagem casa e móveis" className='img-category'/>
-            <p className="title-category">Casa e Móveis</p>
-          </div>
-
-        </div>
-
-        <div id="categorys">
-
-          <div id='clothing' className="content-category" ref={inputClothing}>
-
-            {clothing.length === 0? 
-              <p className='no-product'>Não há nenhum produto nessa categoria</p>
-            :
-              ''
-            }
-
-            {clothing && clothing.map((products) => (
-              <Link to={`/more-information/${products.id}`} key={products.id} className='card-product'>
-                <div className="icons" onClick={(e) => e.preventDefault()}>
-                  {!products.emphasis &&
-                    <i className="bi bi-star" onClick={() => handleEmphasis(products.emphasis, products.id)}></i>
-                  }
-                  {products.emphasis &&
-                    <i className="bi bi-star-fill" onClick={() => handleEmphasis(products.emphasis, products.id)}></i>
-                  }
-                  <i className="bi bi-trash3-fill" onClick={() => handleDelete(products.id)}></i>
-                </div>
-                <div className="img-product" style={{
-                  background: `white url(${products.photo}) center center no-repeat`,
-                  backgroundSize: 'contain'
-                }}></div>
-                <p className="title-product">{products.name}</p>
-                <p className='price-product'>R${products.price}</p>
-              </Link>
-            ))}
-          </div>
-
-          <div id="tools" className="content-category" ref={inputTools}>
-
-            {tools.length === 0? 
-              <p className='no-product'>Não há nenhum produto nessa categoria</p>
-            :
-              ''
-            }
-
-            {tools && tools.map((products) => (
+          <div id="categorys">
+            <div id='clothing' className="content-category" ref={inputClothing}>
+              {clothing.length === 0?
+                <p className='no-product'>Não há nenhum produto nessa categoria</p>
+              :
+                ''
+              }
+              {clothing && clothing.map((products) => (
                 <Link to={`/more-information/${products.id}`} key={products.id} className='card-product'>
                   <div className="icons" onClick={(e) => e.preventDefault()}>
                     {!products.emphasis &&
-                     <i className="bi bi-star" onClick={() => handleEmphasis(products.emphasis, products.id)}></i>
+                      <i className="bi bi-star" onClick={() => handleEmphasis(products.emphasis, products.id)}></i>
                     }
                     {products.emphasis &&
-                     <i className="bi bi-star-fill" onClick={() => handleEmphasis(products.emphasis, products.id)}></i>
+                      <i className="bi bi-star-fill" onClick={() => handleEmphasis(products.emphasis, products.id)}></i>
                     }
                     <i className="bi bi-trash3-fill" onClick={() => handleDelete(products.id)}></i>
                   </div>
                   <div className="img-product" style={{
                     background: `white url(${products.photo}) center center no-repeat`,
                     backgroundSize: 'contain'
-                    }}></div>
+                  }}></div>
                   <p className="title-product">{products.name}</p>
                   <p className='price-product'>R${products.price}</p>
-              </Link>
-            ))}
-          </div>
+                </Link>
+              ))}
+            </div>
 
-          <div id="technology" className="content-category" ref={inputTechnology}>
+            <div id="tools" className="content-category" ref={inputTools}>
+              {tools.length === 0?
+                <p className='no-product'>Não há nenhum produto nessa categoria</p>
+              :
+                ''
+              }
+              {tools && tools.map((products) => (
+                  <Link to={`/more-information/${products.id}`} key={products.id} className='card-product'>
+                    <div className="icons" onClick={(e) => e.preventDefault()}>
+                      {!products.emphasis &&
+                       <i className="bi bi-star" onClick={() => handleEmphasis(products.emphasis, products.id)}></i>
+                      }
+                      {products.emphasis &&
+                       <i className="bi bi-star-fill" onClick={() => handleEmphasis(products.emphasis, products.id)}></i>
+                      }
+                      <i className="bi bi-trash3-fill" onClick={() => handleDelete(products.id)}></i>
+                    </div>
+                    <div className="img-product" style={{
+                      background: `white url(${products.photo}) center center no-repeat`,
+                      backgroundSize: 'contain'
+                      }}></div>
+                    <p className="title-product">{products.name}</p>
+                    <p className='price-product'>R${products.price}</p>
+                </Link>
+              ))}
+            </div>
 
-            {technology.length === 0? 
-              <p className='no-product'>Não há nenhum produto nessa categoria</p>
-            :
-              ''
-            }
+            <div id="technology" className="content-category" ref={inputTechnology}>
+              {technology.length === 0?
+                <p className='no-product'>Não há nenhum produto nessa categoria</p>
+              :
+                ''
+              }
+              {technology && technology.map((products) => (
+                  <Link to={`/more-information/${products.id}`} key={products.id} className='card-product'>
+                    <div className="icons" onClick={(e) => e.preventDefault()}>
+                      {!products.emphasis &&
+                       <i className="bi bi-star" onClick={() => handleEmphasis(products.emphasis, products.id)}></i>
+                      }
+                      {products.emphasis &&
+                       <i className="bi bi-star-fill" onClick={() => handleEmphasis(products.emphasis, products.id)}></i>
+                      }
+                      <i className="bi bi-trash3-fill" onClick={() => handleDelete(products.id)}></i>
+                    </div>
+                    <div className="img-product" style={{
+                      background: `white url(${products.photo}) center center no-repeat`,
+                      backgroundSize: 'contain'
+                      }}></div>
+                    <p className="title-product">{products.name}</p>
+                    <p className='price-product'>R${products.price}</p>
+                </Link>
+              ))}
+            </div>
 
-            {technology && technology.map((products) => (
-                <Link to={`/more-information/${products.id}`} key={products.id} className='card-product'>
-                  <div className="icons" onClick={(e) => e.preventDefault()}>
-                    {!products.emphasis &&
-                     <i className="bi bi-star" onClick={() => handleEmphasis(products.emphasis, products.id)}></i>
-                    }
-                    {products.emphasis &&
-                     <i className="bi bi-star-fill" onClick={() => handleEmphasis(products.emphasis, products.id)}></i>
-                    }
-                    <i className="bi bi-trash3-fill" onClick={() => handleDelete(products.id)}></i>
-                  </div>
-                  <div className="img-product" style={{
-                    background: `white url(${products.photo}) center center no-repeat`,
-                    backgroundSize: 'contain'
-                    }}></div>
-                  <p className="title-product">{products.name}</p>
-                  <p className='price-product'>R${products.price}</p>
-              </Link>
-            ))}
-          </div>
-
-          <div id="sport" className="content-category" ref={inputSport}>
-
-            {sport.length === 0? 
-              <p className='no-product'>Não há nenhum produto nessa categoria</p>
-            :
-              ''
-            }
-
-            {sport && sport.map((products) => (
-                <Link to={`/more-information/${products.id}`} key={products.id} className='card-product'>
-                  <div className="icons" onClick={(e) => e.preventDefault()}>
-                    {!products.emphasis &&
-                     <i className="bi bi-star" onClick={() => handleEmphasis(products.emphasis, products.id)}></i>
-                    }
-                    {products.emphasis &&
-                     <i className="bi bi-star-fill" onClick={() => handleEmphasis(products.emphasis, products.id)} ></i>
-                    }
-                    <i className="bi bi-trash3-fill" onClick={() => handleDelete(products.id)}></i>
-                  </div>
-                  <div className="img-product" style={{
-                    background: `white url(${products.photo}) center center no-repeat`,
-                    backgroundSize: 'contain'
-                    }}></div>
-                  <p className="title-product">{products.name}</p>
-                  <p className='price-product'>R${products.price}</p>
-              </Link>
-            ))}
-          </div>
-
-          <div id="house" className="content-category" ref={inputHouse}>
-
-            {house.length === 0? 
-              <p className='no-product'>Não há nenhum produto nessa categoria</p>
-            :
-              ''
-            }
-
-            {house && house.map((products) => (
-                <Link to={`/more-information/${products.id}`} key={products.id} className='card-product'>
-                  <div className="icons" onClick={(e) => e.preventDefault()}>
-                    {!products.emphasis &&
-                     <i className="bi bi-star" onClick={() => handleEmphasis(products.emphasis, products.id)}></i>
-                    }
-                    {products.emphasis &&
-                     <i className="bi bi-star-fill" onClick={() => handleEmphasis(products.emphasis, products.id)}></i>
-                    }
-                    <i className="bi bi-trash3-fill" onClick={() => handleDelete(products.id)}></i>
-                  </div>
-                  <div className="img-product" style={{
-                    background: `white url(${products.photo}) center center no-repeat`,
-                    backgroundSize: 'contain'
-                    }}></div>
-                  <p className="title-product">{products.name}</p>
-                  <p className='price-product'>R${products.price}</p>
-              </Link>
-            ))}
-          </div>
+            <div id="sport" className="content-category" ref={inputSport}>
+              {sport.length === 0?
+                <p className='no-product'>Não há nenhum produto nessa categoria</p>
+              :
+                ''
+              }
+              {sport && sport.map((products) => (
+                  <Link to={`/more-information/${products.id}`} key={products.id} className='card-product'>
+                    <div className="icons" onClick={(e) => e.preventDefault()}>
+                      {!products.emphasis &&
+                       <i className="bi bi-star" onClick={() => handleEmphasis(products.emphasis, products.id)}></i>
+                      }
+                      {products.emphasis &&
+                       <i className="bi bi-star-fill" onClick={() => handleEmphasis(products.emphasis, products.id)} ></i>
+                      }
+                      <i className="bi bi-trash3-fill" onClick={() => handleDelete(products.id)}></i>
+                    </div>
+                    <div className="img-product" style={{
+                      background: `white url(${products.photo}) center center no-repeat`,
+                      backgroundSize: 'contain'
+                      }}></div>
+                    <p className="title-product">{products.name}</p>
+                    <p className='price-product'>R${products.price}</p>
+                </Link>
+              ))}
+            </div>
+            
+            <div id="house" className="content-category" ref={inputHouse}>
+              {house.length === 0?
+                <p className='no-product'>Não há nenhum produto nessa categoria</p>
+              :
+                ''
+              }
+              {house && house.map((products) => (
+                  <Link to={`/more-information/${products.id}`} key={products.id} className='card-product'>
+                    <div className="icons" onClick={(e) => e.preventDefault()}>
+                      {!products.emphasis &&
+                       <i className="bi bi-star" onClick={() => handleEmphasis(products.emphasis, products.id)}></i>
+                      }
+                      {products.emphasis &&
+                       <i className="bi bi-star-fill" onClick={() => handleEmphasis(products.emphasis, products.id)}></i>
+                      }
+                      <i className="bi bi-trash3-fill" onClick={() => handleDelete(products.id)}></i>
+                    </div>
+                    <div className="img-product" style={{
+                      background: `white url(${products.photo}) center center no-repeat`,
+                      backgroundSize: 'contain'
+                      }}></div>
+                    <p className="title-product">{products.name}</p>
+                    <p className='price-product'>R${products.price}</p>
+                </Link>
+              ))}
+            </div>
           
+          </div>
         </div>
       </main>
       <Footer></Footer>
